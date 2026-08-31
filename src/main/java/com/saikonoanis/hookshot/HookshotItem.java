26716c1f30df41531046393b0f496014a219a4d3
1nis <anis@kherraf.fr>
2026-08-31 21:13:38 +0200

    Ajout système Pull / Push Grappin et debug côté serveur. Suppression utilisation côté client seulement.

--- src/main/java/com/saikonoanis/hookshot/HookshotItem.java
+++ src/main/java/com/saikonoanis/hookshot/HookshotItem.java
@@ -1,12 +1,20 @@
 package com.saikonoanis.hookshot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
 import net.minecraft.util.math.Vec3d;
 
 public class HookshotItem extends Item {
+    double distanceMax = 100;
+    float tickDelta = 1.0F;
+    boolean includeFluids = false;
+    Vec3d pointCible;
 
     public HookshotItem(Settings settings) {
         super(settings);
@@ -14,8 +22,32 @@ public class HookshotItem extends Item {
 
     @Override
     public ActionResult use(World world, PlayerEntity user, Hand hand) {
-        Vec3d vecteur = user.getRotationVector();
-        user.setVelocity(vecteur.multiply(3));
+
+        if (world.isClient()) {
+            return ActionResult.PASS;
+        }
+
+        if (pointCible == null) {
+            HitResult resultat;
+            resultat = user.raycast(distanceMax, tickDelta, includeFluids);
+            if (resultat.getType() == HitResult.Type.BLOCK) {
+                pointCible = resultat.getPos();
+                user.sendMessage(Text.of("Grappin lancé"), false);
+            }
+
+        } else {
+            Vec3d posJoueur = user.getEntityPos();
+            Vec3d direction = pointCible.subtract(posJoueur);
+            direction = direction.normalize();
+            user.setVelocity(direction.multiply(5));
+            user.velocityDirty = true;
+            if (user instanceof ServerPlayerEntity serverPlayer) {
+                serverPlayer.networkHandler.sendPacket(new EntityVelocityUpdateS2CPacket(user));
+            }
+            user.sendMessage(Text.of("Grappin tiré"), false);
+            pointCible = null;
+        }
+
         return ActionResult.PASS;
     }
 
